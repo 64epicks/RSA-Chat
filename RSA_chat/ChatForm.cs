15dbc3fc9e7c591fd5287b8f650a41ad046c04a9
AlastairP <jacob_paul@outlook.com>
2018-05-03 14:41:17 +0200

    h2

--- RSA_chat/ChatForm.cs
+++ RSA_chat/ChatForm.cs
@@ -164,11 +164,11 @@ namespace RSA_chat
                     {
                         int bitlength = 2048;
 
-                        //rtbChat.Text += "Success!\n";
+                        rtbChat.Text += "Success!\n";
                         if (est == false)
                         {
                             est = true;
-                            //rtbChat.Text += "Generating " + bitlength + "-bit key...\n";
+                            rtbChat.Text += "Generating " + bitlength + "-bit key...\n";
 
                             #region Key generation and exchange
 
@@ -209,12 +209,12 @@ namespace RSA_chat
                             string keyToSend = "K" + pubKeyString;
                             byte[] keydata = Encoding.ASCII.GetBytes(keyToSend);
 
-                            //rtbChat.Text += "Sending public key to remote...\n";
+                            rtbChat.Text += "Sending public key to remote...\n";
 
                             sendingClient.Send(keydata, keydata.Length);
 
                             #endregion
-                            //rtbChat.Text += "Success!\n";
+                            rtbChat.Text += "Success!\n";
                             
                         }
                     }
@@ -271,10 +271,10 @@ namespace RSA_chat
                 {
                     ready = true;
                     remotePubKeyString = message.Substring(1);
-                    //rtbChat.Text += "Remote accepted\n";
-                    //rtbChat.Text += "Remote public key is: " + remotePubKeyString + "\n";
-                    //rtbChat.Text += "Your public key is: " + pubKeyString + "\n";
-                    //rtbChat.Text += "Your private key is: " + privKeyString + "\n";
+                    rtbChat.Text += "Remote accepted\n";
+                    rtbChat.Text += "Remote public key is: " + remotePubKeyString + "\n";
+                    rtbChat.Text += "Your public key is: " + pubKeyString + "\n";
+                    rtbChat.Text += "Your private key is: " + privKeyString + "\n";
                 }
                 if (messageType == "T")
                 {
